2325752e4a37ef7890e27930b9b1157fe4e01144
746-nia <{irnika}>
2023-11-27 22:03:37 +0700

    ДЗ 8 выполнено. Исправлено

--- src/main/java/ru/otus/nikolaeva/homework8/Main.java
+++ src/main/java/ru/otus/nikolaeva/homework8/Main.java
@@ -3,7 +3,7 @@ package ru.otus.nikolaeva.homework8;
 public class Main {
     public static void main(String[] args) {
         try{
-            arrayCheckSize(new String[][] {{"fdf", "5", "4", "5"},{"4", "5", "4", "5"},{"4", "5", "4", "5"},{"4", "5", "4", "5"}});
+            arrayCheckSize(new String[][] {{"fdf", "5", "4", "5", "5"},{"4", "5", "4", "5"},{"4", "5", "4", "5"},{"4", "5", "4", "5"}});
         }catch (AppArraySizeExeption ex){
             System.out.println(ex.getMessage());
         }
@@ -13,13 +13,13 @@ public class Main {
     }
     public static  int arrayCheckSize(String[][] arr) throws AppArraySizeExeption, AppArrayDataExeption{
        int sum = 0;
-       if(arr.length != 4 || arr[0].length != 4 || arr[1].length != 4 || arr[2].length != 4 || arr[3].length != 4){
-           throw new AppArraySizeExeption("Размер массива не соответствует " + arr.length);
-       }
+
         for (int i = 0; i < arr.length ; i++) {
             for (int j = 0; j < arr[i].length ; j++) {
+                if(arr[i].length != 4 || arr[j].length != 4){
+                    throw new AppArraySizeExeption("Размер массива не соответствует " + arr.length);
+                }
                 try{
-                    Integer.parseInt(arr[i][j]);
                     sum +=Integer.parseInt(arr[i][j]);
                 }catch (NumberFormatException e){
                     throw new AppArrayDataExeption("Некоректный формат преобразования " + i + " : "+ j);
